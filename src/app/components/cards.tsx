04aebed165de0ff146819b9a3029e8c2ff6fbed2
Devon107 <abiclank@gmail.com>
2024-04-09 16:45:25 -0600

    Drag and drop move with full opacity

--- src/app/components/cards.tsx
+++ src/app/components/cards.tsx
@@ -6,16 +6,39 @@ type CardProps = {
     status: string;
     onDragStart: (event: any, cardId: number) => void;
     onDragEnd: (event: any) => void;
+    isDragged: boolean;
+    dx: number;
+    dy: number;
+    dragSize: {w: number, h: number};
+    dragIndicator: string | null;
 }
-const Cards = ({ title, description, href, id, status, onDragStart, onDragEnd}: CardProps) => {
+const Cards = ({ title, description, href, id, status, onDragStart, onDragEnd, isDragged, dx, dy, dragSize, dragIndicator}: CardProps) => {
     if(!id) return null
     return (
-        <div className="bg-slate-600 p-4 mx-2 rounded" draggable onDragStart={(event:any) => onDragStart(event, id)} onDragEnd={onDragEnd}>
-            <h3>{title}</h3>
-            <p>{description}</p>
-            <a href={href && ""}>Read more</a>
-            <p>Status: {status}</p>
-        </div>
+        <>
+            <div className="bg-slate-600 p-4 mx-2 rounded cursor-grab active:cursor-grabbing" style={{zIndex: 2}} draggable onDragStart={(event:any) => onDragStart(event, id)} onDragEnd={onDragEnd}>
+                <h3>{title}</h3>
+                <p>{description}</p>
+                <a href={href && ""}>Read more</a>
+                <p>Status: {status}</p>
+            </div>
+            {isDragged && 
+                <div className={`ghost-${id} bg-slate-600 p-4 mx-2 rounded ${dragIndicator === id.toString() ? "block" : "hidden"}`} style={{
+                    position: "absolute",
+                    transform: `translate(${dx}px, ${dy}px)`,
+                    height: `${dragSize.h}px`,
+                    width: `${dragSize.w}px`,
+                    top: 0,
+                    left: 0,
+                    zIndex: 1
+                }}>
+                    <h3>{title}</h3>
+                    <p>{description}</p>
+                    <a href={href && ""}>Read more</a>
+                    <p>Status: {status}</p>
+                </div>
+            }
+        </>
     );
 };
 
--- src/app/dashboard/page.tsx
+++ src/app/dashboard/page.tsx
@@ -15,26 +15,68 @@ interface Card {
 export default function Dashboard() {
     const [card, setCard] = useState<Card[]>([]);
     const [dropIndicator, setDropIndicator] = useState<string | null>(null)
+    const [dragIndicator, setDragIndicator] = useState<string | null>(null)
     const [showForm, setShowForm] = useState<boolean>(false)
+    const [isDragged, setIsDragged] = useState<boolean>(false)
+    const [dragSize, setDragSize] = useState({
+        w: 0,
+        h: 0,
+    });
+    const [{ dx, dy }, setOffset] = useState({
+        dx: 0,
+        dy: 0,
+    });
     const handleClick = (e: any) => {
         e.preventDefault()
         e.stopPropagation()
         setShowForm(!showForm)
     }
     const handleDragStart = (event: any, cardId: number) => {
-        const section = event.currentTarget;
-        event.dataTransfer.setDragImage(section, event.nativeEvent.offsetX, event.nativeEvent.offsetY);
+        setIsDragged(true)
+        setDragIndicator(cardId.toString())
+        const section = document.createElement("div");
+        section.id = "section";
+        section.classList.add("ghost-"+cardId);
+        section.classList.add("dragging--hidden");
+        document.body.appendChild(section);
+        event.dataTransfer.setDragImage(section, 0, 0);
         event.dataTransfer.setData("section", cardId.toString())
-        section.style.border = "1px solid";
+        const section1 = event.currentTarget;
+        section1.style.border = "1px solid";
+        setOffset ({
+            dx: event.clientX,
+            dy: event.clientY,
+        })
+        setDragSize({
+            w: section1.offsetWidth,
+            h: section1.offsetHeight
+        })
+        const handleDrag = (event: any) => {
+            setOffset({
+                dx: event.clientX,
+                dy: event.clientY,
+            })
+        }
+        const handleDragEnd2 = () => {
+            section1.removeEventListener('drag', handleDrag);
+            section1.removeEventListener('dragend', handleDragEnd);
+            section.remove();
+            setIsDragged(false);
+        };
+        section1.addEventListener("drag", handleDrag);
+        section1.addEventListener("dragend", handleDragEnd2);
     }
     const handleDragEnd = (event: any) => {
+        setIsDragged(false)
         event.dataTransfer.clearData()
         setDropIndicator(null)
-        const section = event.currentTarget;
-        section.style.border = "none";
+        const section1 = event.currentTarget;
+        section1.style.border = "none";
     }
     const handleDrop = (event: any, status : string) => {
         event.preventDefault();
+        const section = document.getElementById("section");
+        if(section) section.remove();
         const cardId = event.dataTransfer.getData("section")
         const newcard = card?.find((_card) => +_card.id === +cardId)
         if(newcard){
@@ -62,6 +104,11 @@ export default function Dashboard() {
             status={_card.status}
             onDragStart={(event:any) => handleDragStart(event, _card.id)}
             onDragEnd={handleDragEnd}
+            isDragged={isDragged}
+            dx={dx}
+            dy={dy}
+            dragSize={dragSize}
+            dragIndicator={dragIndicator}
         />
         ))
     }
@@ -71,13 +118,13 @@ export default function Dashboard() {
             <h2>Todo</h2>
             <h2>In Progress</h2>
             <h2>Done</h2>
-            <section id="todo" onDragOver={handleDragOver} onDrop={(e) => handleDrop(e, "todo")} className="bg-slate-800 p-4 mx-2 rounded flex flex-col gap-2">
+            <section id="todo" onDragOver={handleDragOver} onDrop={(e) => handleDrop(e, "todo")} className={`p-4 mx-2 rounded flex flex-col gap-2 ${dropIndicator === "todo" ? "bg-slate-700" : "bg-slate-800"}`}>
                 {renderCard("todo")}
             </section>
-            <section id="in-progress" onDragOver={handleDragOver} onDrop={(e) => handleDrop(e, "in-progress")}  className="bg-slate-800 p-4 mx-2 rounded flex flex-col gap-2">
+            <section id="in-progress" onDragOver={handleDragOver} onDrop={(e) => handleDrop(e, "in-progress")}  className={`p-4 mx-2 rounded flex flex-col gap-2 ${dropIndicator === "in-progress" ? "bg-slate-700" : "bg-slate-800"}`}>
                 {renderCard("in-progress")}
             </section>
-            <section id="done" onDragOver={handleDragOver} onDrop={(e) => handleDrop(e, "done")}  className="bg-slate-800 p-4 mx-2 rounded flex flex-col gap-2">
+            <section id="done" onDragOver={handleDragOver} onDrop={(e) => handleDrop(e, "done")}  className={`p-4 mx-2 rounded flex flex-col gap-2 ${dropIndicator === "done" ? "bg-slate-700" : "bg-slate-800"}`}>
                 {renderCard("done")}
             </section>
         </div>
